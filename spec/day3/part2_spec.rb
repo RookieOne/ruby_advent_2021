b013e617b6cb0f7ded51c29dd4f6f83e7079fa1a
Jonathan Birkholz <rookieone@gmail.com>
2022-05-25 09:42:18 -0500

    Day 3 spec

--- spec/day3/part2_spec.rb
+++ spec/day3/part2_spec.rb
@@ -29,17 +29,17 @@ RSpec.describe "Day 3 Part 2" do
     expect(answer[:life_support_rating]).to   eq(230)
   end
 
-  # it "answer from input should be right" do
-  #   input = Day3FileReader.read_file("./spec/day3/input.txt")
+  it "answer from input should be right" do
+    input = Day3FileReader.read_file("./spec/day3/input.txt")
 
-  #   answer = Day3Part1.get_answer(input)
+    answer = Day3Part2.get_answer(input)
 
-  #   expect(answer[:gamma_bit_string]).to   eq("000010111101")
-  #   expect(answer[:epsilon_bit_string]).to   eq("111101000010")
+    expect(answer[:oxygeon_rating]).to   eq("010000101111")
+    expect(answer[:co2_rating]).to   eq("111001111010")
 
-  #   expect(answer[:gamma_rate]).to   eq(189)
-  #   expect(answer[:epsilon_rate]).to   eq(3906)
+    expect(answer[:oxygeon_number]).to   eq(1071)
+    expect(answer[:co2_number]).to   eq(3706)
 
-  #   expect(answer[:power_consumption]).to   eq(738234)
-  # end
+    expect(answer[:life_support_rating]).to   eq(3969126)
+  end
 end
